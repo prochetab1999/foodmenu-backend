720e768195155de4f13afde97024f209c6927512
prochetab1999 <procheta.b@thoughtworks.com>
2023-05-23 18:22:38 +0530

    Add. endpoints for adding food item and getting food items

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import { Schema, connect, model } from 'mongoose';
 
+connect(
+  'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.0',
+  {
+    dbName: 'foodmenu',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+const foodSchema = Schema({
+  name: String,
+  cost: Number,
+});
+const Food = model('Food', foodSchema);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen('8080');
 app.get('/', (req, res) => {
   res.send('FoodMenu Backend is running');
+});
+app.post('/food', async (req, res) => {
+  try {
+    const foodResponse = await new Food({
+      name: req.body.name,
+      cost: req.body.cost,
+    }).save();
+    res.json(foodResponse);
+    res.status(200);
+  } catch {
+    res.send('food item cannot be added');
+  }
+});
+app.get('/food', async (req, res) => {
+  try {
+    const foodResponse = await Food.find({});
+    res.json(foodResponse);
+    res.status(200);
+  } catch (err) {
+    res.send('cannot retrieve food items');
+  }
 });
